34bc437e7616de471a27ca8d9a572a8582d55502
Ruvik <Kerinaguilera@gmail.com>
2025-03-07 18:56:56 +0100

    Mis Primeras practicas en casa

---
+++ MiPrimerPrograma.java
@@ -0,0 +1,12 @@
+public class MiPrimerPrograma {
+    public static void main(String[] args) {
+        //esto es solo texto
+        System.out.println("*******************");
+        System.out.println("Hola Mundo");
+        System.out.println("*******************");
+        System.out.println("Esto es una prueba para programar en Java");
+        System.out.println("Las pruebas seran: "+1000);
+        System.out.println(3);
+        System.out.println(3+2);
+    }
+}
